b941b315350bf50f744d360a12897330cf912641
shoji_naoto <onyxnaoto@gmail.com>
2018-08-17 23:46:24 +0900

    edit-palce holder
    投稿画面の編集

--- blog/app/controllers/blogs_controller.rb
+++ blog/app/controllers/blogs_controller.rb
@@ -26,7 +26,7 @@ class BlogsController < ApplicationController
     end
 
     def move_to_index
-      redirect_to action: :index unless user_signed_in?
+      redirect_to :action => "index" unless user_signed_in?
     end
 
 end
